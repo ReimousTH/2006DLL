5ed2c3945ae574787c57801d900e3689c4a0a158
ReimousTH <51449880+ReimousTH@users.noreply.github.com>
2023-12-21 21:38:13 +0300

    Little

--- 2006DLL/Marathon/Sox/Engine/Task.h
+++ 2006DLL/Marathon/Sox/Engine/Task.h
@@ -24,10 +24,10 @@ namespace Sonicteam{
 			//Fields
 			unsigned int Tuint0x24;
 			unsigned int Tuint0x28;
-			unsigned int Tuint0x2C;
-			unsigned int Tuint0x30;
-			unsigned int Tuint0x34;// CurrentMode (MainMode, and There Others)
-			unsigned int Tuint0x38;// 
+			unsigned int Tuint0x2C; //TASk
+			unsigned int Tuint0x30; //TASK
+			unsigned int Tuint0x34;// CurrentMode (MainMode, and There Others) IGuess Task is IT
+			unsigned int Tuint0x38;// idek
 			Sonicteam::SoX::Engine::Doc* TaskEngineDoc; // Sonicteam::SoX::Engine::Doc ,0x3C
 			Sonicteam::SoX::IObjectNode TaskList;
 			unsigned int Tuint0x48;
